85c7eeeda294d75cb725827607487c9567b43355
Sebastian Murgas Tejeda <smurgast@unicesar.edu.co>
2026-08-17 16:56:46 -0500

    feat: crear clase Persona como padre de Cliente

---
+++ src/main/java/com/mycompany/biblioteca/Person.java
@@ -0,0 +1,31 @@
+package com.mycompany.biblioteca;
+
+public class Person {
+
+    private String id;
+    private String name;
+    private String phone;
+
+    public Person() {
+    }
+
+    public Person(String id, String name, String phone) {
+        this.id = id;
+        this.name = name;
+        this.phone = phone;
+    }
+
+    public String getId() { return id; }
+    public void setId(String id) { this.id = id; }
+
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+
+    public String getPhone() { return phone; }
+    public void setPhone(String phone) { this.phone = phone; }
+
+    @Override
+    public String toString() {
+        return "id=" + id + ", nombre=" + name + ", telefono=" + phone;
+    }
+}
